ea2f5093de68fdc0a2528546a948483d5ac8ec19
raunit-dev <raunitjaiswal510@gmail.com>
2025-04-20 11:12:13 +0530

    added

---
+++ programs/staking/src/instructions/initialize_config.rs
@@ -0,0 +1,44 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint,Token};
+
+
+use crate::StakeConfig;
+
+#[derive(Accounts)]
+pub struct InitializeConfig<'info> {
+    #[account(
+        mut
+    )]
+    pub admin: Signer<'info>,
+    #[account(
+        init,
+        payer = admin,
+        space = InitSpace,
+        seeds = [b"config",admin.key().as_ref()],
+        bump
+    )]
+    pub config: Account<'info,StakeConfig>,
+    #[account(
+        mint::token_program = token_program,
+        seeds = [b"reward",config.key().as_ref()],
+        bump
+    )]
+    pub reward_mint: Account<'info,Mint>,
+    pub token_program: Program<'info,Token>,
+    pub system_program: Program<'info,System>
+}
+
+impl<'info> InitializeConfig<'info> {
+    pub fn initialize_config(&mut self,points_per_stake: u64,max_stake: u64,freeze_period: i64,bumps: &InitializeConfigBumps) -> Result<()> {
+        self.config.set_inner(StakeConfig {
+            rewards_bump,
+            bump: bump,
+            points_per_stake,
+            freeze_period,
+            max_stake
+        });
+
+        Ok(())
+    }
+}
+
--- programs/staking/src/instructions/mod.rs
+++ programs/staking/src/instructions/mod.rs
@@ -1,3 +1,7 @@
-pub mod initialize;
+pub mod initialize_config;
+pub mod initialize_user;
+pub mod stake;
 
-pub use initialize::*;
+pub use initialize_config::*;
+pub use initialize_user::*;
+pub use stake::*;
